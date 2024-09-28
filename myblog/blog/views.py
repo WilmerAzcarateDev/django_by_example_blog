dce7fb050ccd428544642771bf9c8c126747a898
Jhon Wilmer Azcarate Rivera <jhonazcaratework@gmail.com>
2024-09-28 14:48:52 -0500

    Añadimos comentarios a nuestro blog

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 class PostListView(ListView):
@@ -18,8 +19,11 @@ def post_detail(request,year,month,day,post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
+    comments = post.comments.filter(active=True)
     
-    return render(request,'blog/post/detail.html',{'post':post})
+    form = CommentForm()
+    
+    return render(request,'blog/post/detail.html',{'post':post,'form':form,'comments':comments})
 
 def post_share(request,post_id):
     post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
@@ -40,3 +44,16 @@ def post_share(request,post_id):
         form = EmailPostForm()
     
     return render(request,'blog/post/share.html',{'post':post,'form':form,'sent':sent})
+
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    comment = None
+    
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    
+    return render(request,'blog/post/comment.html',{'post':post,'form':form,'comment':comment})
