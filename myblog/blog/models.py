715dadd38a9536d939d377af8ab23cb4676288a3
Jhon Wilmer Azcarate Rivera <jhonazcaratework@gmail.com>
2024-09-28 11:07:56 -0500

    UPDATE - Registramos el modelos post en el dashboard de administracion.

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,12 +1,40 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    
+    """
+        Usamos la clase status como un enum
+    """
+    class Status(models.TextChoices):
+        DRAFT = 'DF','Draft'
+        PUBLISHED = 'PB','Published'
+    
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
     
-    """Transforma el registro
+    """
+        Es la metadata del modelo
+    """
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+    
+    """
+        Transforma el registro en un string
+        en caso de necesitarlo para una vista.
+        En este caso, cuando llamemos un post como
+        string, nos traera el titulo.
     """
     def __str__(self) -> str:
-        return super().__str__()
+        return self.title
